c08d19ec2d0be148b3e9fba9aea8941086c8f925
MrOrga <44905639+MrOrga@users.noreply.github.com>
2019-10-01 18:02:57 +0200

    finish assigment

--- assignment 1/src/PICalc.java
+++ assignment 1/src/PICalc.java
@@ -1,5 +1,5 @@
 /*
-*@author Daniele Orgallo 501516
+ *@author Daniele Orgallo 501516
 
  */
 import java.util.concurrent.*;
@@ -7,40 +7,45 @@ import static java.lang.Math.abs;
 
 public class PICalc implements Runnable
 {
-    double accuracy;
-    double value=0;
+	double accuracy;
+	double value=0;
 
-    PICalc(double accuracy)
-    {
-        this.accuracy = accuracy;
-    }
+	PICalc(double accuracy)
+	{
+		this.accuracy = accuracy;
+	}
 
-    @Override
-    public void run()
-    {
-        double i=1;
-        while(abs(value - Math.PI) > accuracy)
-        {
-            value += 4/i;
-            i=-(abs(i)+2);
-        }
+	@Override
+	public void run()
+	{
+		double i=1;
+		double j=i;
+		while(abs(value - Math.PI) > accuracy && !Thread.currentThread().isInterrupted())
+		{
+			value += 4/(j*(i));
+			i=(i+2);
+			j=-1*j;
+		}
+		if(Thread.currentThread().isInterrupted())
+			System.out.println("Worker interrotto");
+	}
+	public static void main(String[] args)
+	{
 
-    }
+		PICalc calc = new PICalc(Double.parseDouble(args[0]));
+		Thread worker = new Thread(calc);
+		worker.start();
+		try
+		{
+			worker.join(Long.parseLong(args[1]));
+			worker.interrupt();
+			System.out.println("Il valore calcolo fino a quel momento è PI = " + calc.value);
+		}
+		catch (InterruptedException intEx)
+		{
+			intEx.printStackTrace();
+			System.out.println("valore di calcolato fino all interruzione PI = " + calc.value);
 
-    public static void main(String[] args)
-    {
-        PICalc calc = new PICalc(Double.parseDouble(args[0]));
-        Thread worker = new Thread(calc);
-        worker.start();
-        try
-        {
-            Thread.sleep(Long.parseLong(args[1]));
-            worker.interrupt();
-            System.out.println("valore di calcolato fino all interruzione PI = " + calc.value);
-        }
-        catch (InterruptedException intex)
-        {
-            
-        }
-    }
+		}
+	}
 }
