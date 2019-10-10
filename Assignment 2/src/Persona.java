95fc93cfba60aa3e21eacf93f8be5e7b4dc6920d
MrOrga <44905639+MrOrga@users.noreply.github.com>
2019-10-10 13:32:36 +0200

    assigment2 update

---
+++ Assignment 2/src/Persona.java
@@ -0,0 +1,24 @@
+public class Persona implements Runnable
+{
+    private String name;
+    public Persona(String name)
+    {
+        this.name = name;
+    }
+    @Override
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName() + ": Task " + name);
+        try
+        {
+            Long duration = (long)(Math.random()*10);
+            System.out.println(Thread.currentThread().getName() + ": Task " + name + " is doing a task during " + duration + " seconds") ;
+            Thread.sleep(duration);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": Task Finished " + name);
+    }
+}
--- Assignment 2/src/UfficioPostale.java
+++ Assignment 2/src/UfficioPostale.java
@@ -7,15 +7,48 @@ public class UfficioPostale implements Runnable
     private static final int nSportelli=4;
     private static final int timeout=10;
     private ThreadPoolExecutor sportelli;
-    LinkedBlockingQueue salaAttesa;
+    private LinkedBlockingQueue <Persona> salaAttesa;
+    //k è  il parametro per la massima lunghezza della coda e deve essere >0
     public UfficioPostale(int k)
     {
         sportelli = new ThreadPoolExecutor(nSportelli,nSportelli,timeout, TimeUnit.SECONDS,new LinkedBlockingQueue<>(k));
-        salaAttesa= new LinkedBlockingQueue();
+        salaAttesa= new LinkedBlockingQueue<>();
+    }
+    public void arrivoPersona(Persona p)
+    {
+        if(p==null) throw new NullPointerException("Persona non può essere null");
+        else
+        {
+            salaAttesa.add(p);
+            System.out.println(p + " è entrato nella sala d'attesa");
+        }
     }
     @Override
     public void run()
     {
+            try
+            {
+                while(true)
+                {
+                    if (sportelli.getQueue().remainingCapacity() > 0)
+                    {
+                        Persona curr = salaAttesa.take();
+                        sportelli.submit(curr);
+                    }
+                }
+            } catch (InterruptedException ex)
+            {
+                sportelli.shutdown();
+                System.out.println("Chiusura ufficio");
+            }
 
+    }
+    public static void main(String[] args)
+    {
+        int k = Integer.parseInt(args[0]);
+        if(k <= 0) throw new IllegalArgumentException("k deve essere maggiore di 0");
+        UfficioPostale uff = new UfficioPostale(k);
+        Thread ufficio = new Thread(uff);
+        ufficio.start();
     }
 }
